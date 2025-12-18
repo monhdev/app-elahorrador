d65552d122a5c991f7332477361148f6f0c30d00
Mon <monproyectocero@gmail.com>
2025-12-19 00:24:46 +0100

    Obfuscate Firebase configuration in production environment
    Updated Firebase configuration values to obfuscate sensitive information.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,14 @@
 export const environment = {
   production: true,
   firebaseConfig : {
-  projectId: "ahorradordatos",
-  appId: "1:183975969634:web:296efa35718cdbc144223b",
+  projectId: "xxxxxxxxx",
+  appId: "xxxxxxxxxxxxxxxxxx",
   storageBucket: "ahorradordatos.firebasestorage.app",
-  apiKey: "AIzaSyBrLNwEpaOdkU4T30Z5p3oEXnYmi3X0uR8",
+  apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxx",
   authDomain: "ahorradordatos.firebaseapp.com",
-  messagingSenderId: "183975969634",
-  measurementId: "G-1018LHD7N3",
-  projectNumber: "183975969634",
+  messagingSenderId: "xxxxxxxxxx",
+  measurementId: "xxxxxxxxxxx",
+  projectNumber: "xxxxxxxxxxxx",
   version: "2"
 },
 };
